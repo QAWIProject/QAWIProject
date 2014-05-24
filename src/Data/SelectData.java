182be4773410cd4364c759015091f8ae9c556505
Ivan Klarman <klarman.ivan@gmail.com>
2014-05-24 16:53:01 +0200

    Script Insertion dynamique + Script Selection
    Script InsertData :
    
    Créer la requête d'insertion automatiquement et envois en base de
    donnée.
    
    Script SelectData :
    Sélectionne et affiche une requête de sélection en paramètre.

---
+++ src/Data/SelectData.java
@@ -0,0 +1,33 @@
+package Data;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class SelectData {
+	private String resultatReq = "";
+	public SelectData(Connection oConnection,String req){
+		try{
+			Statement st = oConnection.createStatement();
+			ResultSet rs = st.executeQuery(req);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			boolean prochain = rs.next();
+			while (prochain){
+				for (int i = 1; i <= rsmd.getColumnCount(); i++){
+					this.resultatReq += rs.getString(i)+" ";
+				}
+				prochain = rs.next();
+			}
+		    st.close();
+		}catch(Exception e){
+			System.out.println("Erreur d'insertion");
+		}
+	}
+	public String getResultatReq() {
+		return resultatReq;
+	}
+	public void setResultatReq(String resultatReq) {
+		this.resultatReq = resultatReq;
+	}
+}
